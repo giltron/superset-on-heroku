9b2937f17a36a430b9f8309bdbf64bb17846505f
Shawn <giltron@gmail.com>
2018-07-18 00:17:25 -0400

    Update superset_config.py

--- superset_config.py
+++ superset_config.py
@@ -5,6 +5,8 @@ import os
 # ROW_LIMIT = 5000
 SUPERSET_WORKERS = 1 # for it to work in heroku basic/hobby dynos increase as you like
 
+AUTH_ROLE_PUBLIC = 'Public'
+
 # SUPERSET_WEBSERVER_PORT = 8088
 #---------------------------------------------------------
 
